178dc4a6946f77b2b2d869bbb7203621173b8a57
HuangSiman <sh894@mail.missouri.edu>
2015-12-01 21:00:47 -0600

    mi

--- main.m
+++ main.m
@@ -1,7 +1,10 @@
 function main()
     %data=readData();
     data1=load('Activity Recognition from Single Chest-Mounted Accelerometer/15.csv');
+    %7 labels
     targets=[1,2,3,4,5,6,7];
+    %return the index returns an array containing the lowest absolute index 
+    %in the last column of input data 
     [~,idx]=ismember(targets,data1(:,5));
     idx=[idx length(data1(:,5))];
     
@@ -10,13 +13,17 @@ function main()
 %         testdata=data1(idx(i):idx(i+1),3);
 %         featureView(testdata(1:100),i)
 %     end
+    %x,y,z acceleration for the respectivily index
     testdata=data1(idx(2):idx(2+1),2:4);
+    %using Fourier transform to generate the features
     f=featureGenerate(testdata);
-    mean(f)
+    %mean of generated feature 
+    m2 = mean(f);
+    featureView(testdata(1:100),2);
     
     testdata=data1(idx(1):idx(1+1),2:4);
     f=featureGenerate(testdata);
-    mean(f)
+    m1 = mean(f);
     
 end
 
@@ -26,12 +33,14 @@ end
 
 function featureView(sequence,i)
     figure(i)
+    %the first graph is the row data
     subplot(3,1,1)
     plot(sequence)
     Y=fft(sequence);
+    %the second graph is the result of Fourier transform
     subplot(3,1,2)
     plot(Y,'ro')
-    
+    %
     subplot(3,1,3)
     n = length(Y);
     power = abs(Y(1:floor(n/2))).^2;
